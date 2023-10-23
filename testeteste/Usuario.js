c9117b7ac9d0392e2d17f1863ba92b9c9d5db98d
Gh0st4v0 <89959988+Gh0st4v0@users.noreply.github.com>
2023-10-23 05:57:22 -0300

    Update Usuario.js

--- testeteste/Usuario.js
+++ testeteste/Usuario.js
@@ -1,119 +1,137 @@
-// this is the model for the class user, it is supposed to store the class's atributes, getters and setters
-// functions and etc.
+const users = [];
 class User {
-    constructor(username, email, password, userLevel, uniqueId, jobRole, fullName, birthday) {
+    constructor(username, email, password, userLevel, jobRole, fullName, birthday) {
       this.username = username;
       this.email = email;
       this.password = password;
       this.userLevel = userLevel;
-      this.uniqueId = uniqueId;
+      this.uniqueId = User.getNextUniqueId();
       this.jobRole = jobRole;
       this.fullName = fullName;
       this.birthday = birthday;
     }
-
-
-  // Getters
-  getUsername() {
-    return this.username;
-  }
-
-  getEmail() {
-    return this.email;
-  }
-
-  getPassword() {
-    return this.password;
-  }
-
-  getUserLevel() {
-    return this.userLevel;
-  }
-
-  getUniqueId() {
-    return this.uniqueId;
-  }
-
-  getJobRole() {
-    return this.jobRole;
-  }
-
-  getFullName() {
-    return this.fullName;
-  }
-
-  getBirthday() {
-    return this.birthday;
-  }
-
-  // Setters
-  setUsername(username) {
-    this.username = username;
-  }
-
-  setEmail(email) {
-    this.email = email;
-  }
-
-  setPassword(password) {
-    this.password = password;
-  }
-
-  setUserLevel(userLevel) {
-    this.userLevel = userLevel;
-  }
-
-  setUniqueId(uniqueId) {
-    this.uniqueId = uniqueId;
-  }
-
-  setJobRole(jobRole) {
-    this.jobRole = jobRole;
-  }
-
-  setFullName(fullName) {
-    this.fullName = fullName;
-  }
-
-  setBirthday(birthday) {
-    this.birthday = birthday;
-  }
   
-    static createUser(username, email, password, userLevel, uniqueId, jobRole, fullName, birthday) {
+    // Getters
+    getUsername() {
+      return this.username;
+    }
+  
+    getEmail() {
+      return this.email;
+    }
+  
+    getPassword() {
+      return this.password;
+    }
+  
+    getUserLevel() {
+      return this.userLevel;
+    }
+  
+    getUniqueId() {
+      return this.uniqueId;
+    }
+  
+    getJobRole() {
+      return this.jobRole;
+    }
+  
+    getFullName() {
+      return this.fullName;
+    }
+  
+    getBirthday() {
+      return this.birthday;
+    }
+  
+    // Setters
+    setUsername(username) {
+      this.username = username;
+    }
+  
+    setEmail(email) {
+      this.email = email;
+    }
+  
+    setPassword(password) {
+      this.password = password;
+    }
+  
+    setUserLevel(userLevel) {
+      // Allow only predefined user levels: 'master', 'admin', 'user'
+      const allowedLevels = ['master', 'admin', 'user'];
+      if (!allowedLevels.includes(userLevel)) {
+        console.error('Invalid user level.');
+        return;
+      }
+  
+      // Prevent setting 'master' user level
+      if (this.userLevel === 'master') {
+        console.error('Cannot change the user level of a master user.');
+        return;
+      }
+  
+      this.userLevel = userLevel;
+    }
+  
+    static getNextUniqueId() {
+        if (typeof User.uniqueId === 'undefined') {
+          User.uniqueId = 1; // Initialize the unique ID
+        } else {
+          User.uniqueId += 1; // Increment the unique ID for each new user
+        }
+        return User.uniqueId;
+      }
+  
+    setJobRole(jobRole) {
+      this.jobRole = jobRole;
+    }
+  
+    setFullName(fullName) {
+      this.fullName = fullName;
+    }
+  
+    setBirthday(birthday) {
+      this.birthday = birthday;
+    }
+  
+    // Static method to create a user
+    static createUser(username, email, password, userLevel, jobRole, fullName, birthday) {
       // Allow only predefined user levels: 'master', 'admin', 'user'
       const allowedLevels = ['master', 'admin', 'user'];
       if (!allowedLevels.includes(userLevel)) {
         console.error('Invalid user level.');
         return null;
       }
-  
-      // Prevent creation of 'master' user outside the code
-      if (userLevel === 'master') {
-        console.error('Cannot create a master user during runtime.');
-        return null;
-      }
-  
-      return new User(username, email, password, userLevel, uniqueId, jobRole, fullName, birthday);
+       // Add the newly created user to the 'users' array
+        const newUser = new User(username, email, password, userLevel, jobRole, fullName, birthday);
+        users.push(newUser);
+      return newUser;
     }
   
-    deleteUser(user) {
-      if (this.userLevel !== 'master') {
+    // Method to delete a user
+    static deleteUser(loggedInUser,user) {
+        console.log('funcao comecou',user)
+      if (loggedInUser.userLevel !== 'master') {
         console.error('Insufficient privileges to delete a user.');
-        return null;
-      }
-  
-      // Logic to delete the user
+        return;
+      } 
+        const index = users.indexOf(user);
+        if (index !== -1) {
+          users.splice(index, 1);
+        }
+      
       console.log(`User '${user.username}' has been deleted.`);
     }
   }
-  
-  // Singleton to handle master user creation
-  const MasterUserSingleton = (() => {
+
+   // Singleton to handle master user creation
+   const MasterUserSingleton = (() => {
     let masterUser = null;
   
     function createMasterUser() {
       if (!masterUser) {
-        masterUser = User.createUser('master', 'master@example.com', 'masterpassword', 'master', '123456', 'Master Role', 'Master User', '1970-01-01');
+        masterUser = User.createUser('master', 'master@example.com', 'masterpassword', 'master', 'Master Role', 'Master User', '1970-01-01');
       }
       return masterUser;
     }
@@ -122,13 +140,14 @@ class User {
       getMasterUser: () => createMasterUser()
     };
   })();
+
+  // Create users
+    User.createUser('master', 'master@example.com', 'masterpassword', 'master', 'Master Role', 'Master User', '1970-01-01'),
+    User.createUser('admin', 'admin@example.com', 'adminpassword', 'admin', 'Admin Role', 'Admin User', '1980-05-15'),
+
+
+exports = {
+    User,
+    users
+}; 
   
-  // Example usage
-  
-  // Create the master user (singleton)
-  const masterUser = MasterUserSingleton.getMasterUser();
-  console.log('Master User:', masterUser);
-  
-  // Attempting to create another master user will return the existing master user instance
-  const duplicateMasterUser = MasterUserSingleton.getMasterUser();
-  console.log('Are master users the same instance?', masterUser === duplicateMasterUser);
